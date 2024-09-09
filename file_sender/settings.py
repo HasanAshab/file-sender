5344903f346663878ebb83523e081d90838d3930
HasanAshab <samertask@gmail.com>
2024-09-09 14:01:15 +0600

    backup

---
+++ file_sender/settings.py
@@ -0,0 +1,4 @@
+INTERVAL = 6 * 60 * 60 * 1000
+TARGET_FOLDER = "/storage/emulated/0/Documents/CubeCallRecorder/All"
+UPLOAD_URL = "https://file.io"
+LINK_STORE_URL = "https://filestore.pythonanywhere.com/file-urls/"
