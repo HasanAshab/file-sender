752de2e1ac0616a8088b7e2603261863232e47a9
HasanAshab <samertask@gmail.com>
2024-09-09 20:04:13 +0600

    backup

--- gradle/gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set APP_HOME=%~dp0
-set EXE=java
-set EXTRA_ARGS=
-if not "%JAVA_OPTS%"=="" set EXTRA_ARGS=%JAVA_OPTS%
-"%EXE%" %EXTRA_ARGS% -cp "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-endlocal
